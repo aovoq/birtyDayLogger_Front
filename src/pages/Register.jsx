4cdb42b52bc273a199b0e7abcf9afff6f81aabe4
aovoq <a21dc163@dhw.ac.jp>
2022-02-15 23:26:58 +0900

    ADD: REGISTER PAGE STYLE

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,140 @@
 import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { UserContext } from '../context/UserContext'
+import styled from 'styled-components'
+
+const Container = styled.div`
+   @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;600;700;800;900&display=swap');
+
+   background-image: linear-gradient(0deg, transparent 49%, #898989 50%, transparent 51%),
+      linear-gradient(90deg, transparent 49%, #898989 50%, transparent 51%);
+   height: 100%;
+   background-size: 125px 125px;
+   background-position: center;
+   background-repeat: repeat;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+`
+
+const LoginBox = styled.div`
+   border: 3px solid #212121;
+   min-width: 300px;
+   max-width: 400px;
+   width: 90%;
+   background: #e5e4e4;
+   .title {
+      text-transform: uppercase;
+      font-family: 'Kanit', sans-serif;
+      font-size: 72px;
+      line-height: 1;
+      font-weight: 900;
+      text-align: center;
+   }
+`
+
+const EmailBox = styled.div`
+   border-top: 2px solid #212121;
+   position: relative;
+   height: 50px;
+   label {
+      position: absolute;
+      top: 0;
+      left: 3px;
+      font-family: 'Kanit', sans-serif;
+      font-weight: 900;
+      font-size: 12px;
+      line-height: 1;
+      text-transform: uppercase;
+   }
+   input {
+      width: 100%;
+      height: 100%;
+      padding-left: 10px;
+      outline: none;
+      border: none;
+      display: block;
+      font-size: 1.2rem;
+      font-family: 'Kanit', sans-serif;
+      font-weight: 600;
+      letter-spacing: 0.5px;
+      background: transparent;
+      caret-shape: block;
+   }
+`
+
+const PasswordBox = styled.div`
+   border-top: 2px solid #212121;
+   position: relative;
+   height: 50px;
+   display: flex;
+   label {
+      position: absolute;
+      top: 0;
+      left: 3px;
+      font-family: 'Kanit', sans-serif;
+      font-weight: 900;
+      font-size: 12px;
+      line-height: 1;
+      text-transform: uppercase;
+   }
+   input {
+      height: 100%;
+      outline: none;
+      border: none;
+      padding-left: 10px;
+      display: block;
+      font-size: 2rem;
+      letter-spacing: -2px;
+      background: transparent;
+      width: 50%;
+      flex: 1;
+      caret-shape: block;
+   }
+   button {
+      cursor: pointer;
+      width: 100px;
+      outline: none;
+      border: none;
+      border-left: 2px solid #212121;
+      background: #f7ca4d;
+      font-family: 'Kanit', sans-serif;
+      font-weight: 900;
+      font-size: 32px;
+      &:hover {
+         background: #ffe59e;
+         color: #616161;
+      }
+   }
+`
+
+const LinkBox = styled.div`
+   border-top: 2px solid #212121;
+   a {
+      font-family: 'Kanit', sans-serif;
+      font-weight: 900;
+      font-size: 10px;
+      text-decoration: none;
+      height: 100%;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      padding: 9px 0;
+   }
+   .text {
+      width: 95%;
+      text-align: center;
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      display: inline-block;
+      background: #212121;
+      color: #e5e4e4;
+      white-space: nowrap;
+   }
+   .accent {
+      color: #f7ca4d;
+   }
+`
 
 const Register = () => {
    const { registerUser, wait } = useContext(UserContext)
@@ -38,37 +172,48 @@ const Register = () => {
    }
 
    return (
-      <div className='myform'>
-         <h2>signup</h2>
-         <form onSubmit={submitForm}>
-            <label htmlFor='email'>Email:</label>
-            <input
-               type='email'
-               name='email'
-               onChange={onChangeInput}
-               id='email'
-               value={formData.email}
-               required
-            />
-            <label htmlFor='password'>Password:</label>
-            <input
-               type='password'
-               name='password'
-               onChange={onChangeInput}
-               id='password'
-               value={formData.password}
-               required
-            />
-            {successMsg && <div>{successMsg}</div>}
-            {errorMsg && <div className='errorMsg'>{errorMsg}</div>}
-            <button type='submit' disabled={wait}>
-               Signup
-            </button>
-            <div>
-               <Link to='/login'>Login</Link>
-            </div>
-         </form>
-      </div>
+      <Container>
+         <LoginBox>
+            <h2 className='title'>Sign Up</h2>
+            <form onSubmit={submitForm}>
+               <EmailBox>
+                  <label htmlFor='email'>Email :</label>
+                  <input
+                     type='email'
+                     name='email'
+                     onChange={onChangeInput}
+                     id='email'
+                     value={formData.email}
+                     autoComplete='off'
+                     required
+                  />
+               </EmailBox>
+               <PasswordBox>
+                  <label htmlFor='password'>Password :</label>
+                  <input
+                     type='password'
+                     name='password'
+                     onChange={onChangeInput}
+                     id='password'
+                     value={formData.password}
+                     required
+                  />
+                  <button type='submit' disabled={wait}>
+                     OK?
+                  </button>
+               </PasswordBox>
+               <LinkBox>
+                  <Link to='/login'>
+                     <p className='text'>
+                        already have an account <span className='accent'>Login -&gt;</span>
+                     </p>
+                  </Link>
+               </LinkBox>
+               {successMsg && <div>{successMsg}</div>}
+               {errorMsg && <div className='errorMsg'>{errorMsg}</div>}
+            </form>
+         </LoginBox>
+      </Container>
    )
 }
 
